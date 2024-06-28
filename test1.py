512e5a09084f1c9f3bc4cb7e56a8b09e428f6550
Faysal_Ezaz <95119493+Faysal-Ezaz@users.noreply.github.com>
2024-06-28 09:46:00 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,9 +1,12 @@
 import boto3
+import os
 
-# Specify the AWS region
-region_name = 'ap-south-1'  # Replace with your desired region
-
-session = boto3.session.Session(region_name=region_name)
+# Create a session using environment variables
+session = boto3.session.Session(
+    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+    region_name="ap-south-1"
+)
 
 ec2 = session.resource('ec2')
 
